e22d02ade909d91f8c5527d9359bef61d184e888
Filipe Moura <filipeasm18@gmail.com>
2022-03-09 21:22:03 -0300

    starts with SOLID principles

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,27 @@
+import { ISpeficicationRepository } from "../repositories/ISpecificationRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationService {
+  constructor(private specificationsRepository: ISpeficicationRepository) {
+
+  }
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists = this.specificationsRepository.findByName(name);
+
+    if(specificationAlreadyExists) {
+      throw new Error('Specification already exists');
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description
+    })
+  }
+}
+
+export { CreateSpecificationService }
